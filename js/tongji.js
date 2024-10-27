98cf9ea50521ee0097d99c6d696c126add10228f
zhanghaoliang <zhanghaoliangbjyw@youedata.com>
2024-10-27 20:50:46 +0800

    change tongji

--- js/tongji.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function () {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?00177971e90f671b254ac12e97a45db3";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
